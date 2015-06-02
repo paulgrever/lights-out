1a853ea1b0fa2849e1427bbe9b31ee22013bf2e4
Paul Grever <paulgrever@gmail.com>
2015-06-02 14:43:30 -0600

    added chai and jquery and set up so now you can run via npm start

--- lights-out.js
+++ lights-out.js
@@ -1,3 +1,5 @@
+var $ = require('jquery');
+require('jquery-ui');
 $(document).ready(function(){
   $(".button-1").on("click", function(){
     console.log("hi");
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-    entry: "./lights-out.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    }
+  entry: "./lights-out.js",
+  output: {
+      path: __dirname,
+      filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' }
+    ]
+  },
 };
