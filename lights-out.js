15f3bd3356e435f29bbccf36a15958960f4ad379
Paul Grever <paulgrever@gmail.com>
2015-06-03 17:41:09 -0600

    added new game which creates x number of rows with y number of columns

--- lights-out.js
+++ lights-out.js
@@ -1,15 +1,21 @@
 var $ = require('jquery');
-var board = require("./game_generator");
+var Board = require('./board');
 
 $(document).ready(function(){
-  $(".button-1").on("click", function(){
-    var fullBoard = board();
-    fullBoard.forEach(function(num){
-      divMaker(fullBoard[num].state);
-    });
+  $(".button-1").on("click", function() {
+    $('.gameboard div').remove();
+    newGame();
   });
 });
-
-var divMaker = function(i){
-  console.log(i);
+var newGame = function(){
+  var board = new Board();
+  var rows = board.length;
+  var columns = board[0].length;
+  for (var r = 0; r < rows; r++) {
+    $('.gameboard').append("<div class=row id=row-"+r+"></div>");
+    for(var c = 0; c < columns; c++){
+      $('#row-'+r).append("<div class='col-md-1 square' id=row-"+r+"column-"+c+">"+ board[r][c].active +"</div>");
+    }
+  }
 };
+newGame();
