0c41d5926866efd5d9357e12f1ea9bd911ea2ab0
Paul Grever <paulgrever@gmail.com>
2015-06-04 17:09:16 -0600

    it changes itself and it's neighbor to the north

--- lights-out.js
+++ lights-out.js
@@ -11,13 +11,57 @@ $(document).ready(function(){
   });
   $(".square").on("click", function () {
     var $id = $(this).attr("id");
-     var space = Game.board[$(this).data("row")][$(this).data("column")];
-      $(this).removeClass(space.active.toString());
+    var row = $(this).data("row");
+    var column = $(this).data("column");
+    var space = Game.board[row][column];
+    $(this).removeClass(space.active.toString());
+    space.active = !space.active;
+    $(this).addClass(space.active.toString());
+    if(space.x !== 0){
 
-      space.active = !space.active;
-      $(this).addClass(space.active.toString());
+      $("#row-" + (space.x - 1) + "column-" + space.y).removeClass(space.board[(space.x-1)][space.y].active.toString());
+      space.board[(space.x-1)][space.y].active = !space.board[(space.x-1)][space.y].active;
+      $("#row-" + (space.x - 1) + "column-" + space.y).addClass(space.board[(space.x-1)][space.y].active.toString());
+    }
   });
 });
+
+
+
+var neighborUpdate = function(row, column){
+  var neighborSpace =[];
+  var rowCount = Game.board.length;
+  var columnCount = Game.board[0].length;
+  // north
+  if (row !==0){
+    neighborSpace.push(Game.board[row - 1][column]);
+  }
+  // west
+  if (column !==0 ){
+    neighborSpace.push(Game.board[row][column - 1]);
+  }
+  return neighborSpace;
+
+  // if((row == 0) && (column === 0)){
+  //   var south = Game.board[row + 1][column];
+  //   var east = Game.board[row][column + 1];
+  //   neighborSpace.push(south);
+  //   neighborSpace.push(east);
+  // } else if (row == 0)  {
+  //   var south = Game.board[row + 1][column];
+  //   var east = Game.board[row][column + 1];
+  //   var west = Game.board[row][column - 1];
+
+  // }
+  // var north = Game.board[row - 1][column];
+  // var south = Game.board[row + 1][column];
+  // var east = Game.board[row][column + 1];
+  // var west = Game.board[row][column - 1];
+  // console.log(north.active);
+  // console.log(east.active);
+  // console.log(south.active);
+  // console.log(west.active);
+};
 
 var game = Game;
 var currentGame = game.newGame();
